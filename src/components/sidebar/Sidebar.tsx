8cef730ad4301b975c336bc5a3f5cb98764ac51f
david waza <idemetoemediong2022@gmail.com>
2024-02-09 20:58:36 +0100

    revamped the sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,25 +1,27 @@
 "use client";
 
-import { FaArrowLeftLong } from "react-icons/fa6";
-import { RiDashboardFill } from "react-icons/ri";
-import { FaToolbox } from "react-icons/fa";
-import { LuUserSquare } from "react-icons/lu";
-import { PiHouseLineFill } from "react-icons/pi";
-import { RiAdminFill } from "react-icons/ri";
-import { GrOrganization } from "react-icons/gr";
-import { IoIosSettings } from "react-icons/io";
-import { IoIosHelpCircle } from "react-icons/io";
 import { MdOutlineArrowDropDown } from "react-icons/md";
 import { CgAddR } from "react-icons/cg";
 import { TiShoppingBag } from "react-icons/ti";
 import { TfiWrite } from "react-icons/tfi";
-import { FaArrowRightLong } from "react-icons/fa6";
 import JapaLoogo from "../../assets/shapes/JapaLogo.svg";
-
 import Link from "next/link";
 import Image from "next/image";
 import { useSide, useSideUpdate } from "./SideContext";
 import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+import { GoBriefcase } from "react-icons/go";
+import { FiUser } from "react-icons/fi";
+import { SlNote } from "react-icons/sl";
+import { GrUserAdmin } from "react-icons/gr";
+import { HiOutlineBuildingLibrary } from "react-icons/hi2";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoIosHelpCircleOutline } from "react-icons/io";
+import { RxCaretDown } from "react-icons/rx";
+
+
 
 export function Sidebar() {
   const [open, setOpen] = useState(false);
@@ -36,25 +38,21 @@ export function Sidebar() {
         currtState ? "w-[18rem]" : "w-[80px]"
       } `}
     >
-      <nav className="flex justify-between gap-4 px-4 py-8 lg:py-6 border-b border-gray-500 overflow-hidden">
+      <nav className="flex justify-between gap-4 px-4 py-8 lg:py-6 border-b border-gray-300 overflow-hidden">
         <Link href="/">
           <Image
             src={JapaLoogo}
             alt="Japa Logo"
-            width={100}
-            height={100}
-            className={`overflow-hidden transition-all ${
-              currtState ? "w-32" : "w-0"
+            width={80}
+            height={80}
+            className={`overflow-hidden transition-all flex justify-center ${
+              currtState ? "w-20" : "w-0"
             }`}
           />
         </Link>
 
-        <button onClick={updateedState} className="p-2 rounded-lg bg-gray-500">
-          {currtState ? (
-            <FaArrowLeftLong color={"lightgray"} size={"1.3rem"} />
-          ) : (
-            <FaArrowRightLong color={"lightgray"} size={"1.2rem"} />
-          )}
+        <button onClick={updateedState} className="p-2 rounded-lg ">
+          {currtState ? <IoMdClose /> : <RxHamburgerMenu />}
         </button>
       </nav>
 
@@ -63,26 +61,29 @@ export function Sidebar() {
       >
         <nav className={`mt-5 py-4 px-4 lg:mt-9 lg:px-6`}>
           <ul className="mb-16 flex flex-col gap-2">
-            <li className="relative flex items-center gap-4 text-xl mb-8">
-              <RiDashboardFill size={"1.5rem"} />
-              <div className={` ${currtState ? "w-full" : "hidden"}`}>
+            <li className="relative flex items-center gap-4 text-[20px] mb-8">
+              <MdOutlineDashboardCustomize size={30} />
+              <div
+                className={` ${currtState ? "w-full" : "hidden"} text-[20px]`}
+              >
                 Dashboard
               </div>
             </li>
-            <li className="relative flex items-center gap-4 text-xl mb-8">
+            <li className="relative flex items-center gap-4 text-[20px] mb-8">
               <button
                 onClick={togleMenu}
                 className="flex flex-col gap-2 items-center"
               >
                 <div className="flex gap-2 items-center">
-                  <FaToolbox size={"1.5rem"} />
+                  <GoBriefcase size={25} />
                   <div
                     className={`flex gap-2 items-center ${
                       currtState ? "w-full" : "hidden"
                     }`}
                   >
                     Job Posting
-                    <MdOutlineArrowDropDown />
+                    <RxCaretDown size={15} />
+
                   </div>
                 </div>
                 <ul className="flex flex-col gap-4 text-left text-sm">
@@ -105,42 +106,43 @@ export function Sidebar() {
                 </ul>
               </button>
             </li>
-            <li className="relative flex items-center gap-4 text-xl mb-8">
-              <LuUserSquare size={"1.6rem"} />
+            <li className="relative flex items-center gap-4 text-[20px] mb-8">
+              <FiUser size={30} />
               <div className={` ${currtState ? "w-full" : "hidden"}`}>
                 users
               </div>
             </li>
-            <li className="relative flex items-center gap-4 text-xl mb-8">
-              <PiHouseLineFill size={"1.5rem"} />
+            <li className="relative flex items-center gap-4 text-[20px] mb-8">
+              <SlNote size={25} />
               <div className={` ${currtState ? "w-full" : "hidden"}`}>
                 Cv Revamp
               </div>
             </li>
-            <li className="relative flex items-center gap-4 text-xl mb-8">
-              <RiAdminFill size={"1.6rem"} />
+            <li className="relative flex items-center gap-4 text-[20px] mb-8">
+              <GrUserAdmin size={30} />
+
               <div className={` ${currtState ? "w-full" : "hidden"}`}>
                 Admins
               </div>
             </li>
-            <li className="relative flex items-center gap-4 text-xl mb-8">
-              <GrOrganization size={"1.6rem"} />
+            <li className="relative flex items-center gap-4 text-[20px] mb-8">
+              <HiOutlineBuildingLibrary size={30} />
               <div className={` ${currtState ? "w-full" : "hidden"}`}>
                 Companies
               </div>
             </li>
+          <hr />
           </ul>
-
           <ul className="mb-16 flex flex-col gap-2">
-            <li className="relative flex items-center gap-4 text-xl mb-8">
-              <IoIosSettings size={"1.7rem"} />
-
+            <li className="relative flex items-center gap-4 text-[20px] mb-8">
+            <IoSettingsOutline size={30}/>
               <div className={` ${currtState ? "w-full" : "hidden"}`}>
                 Settings
               </div>
             </li>
-            <li className="relative flex items-center gap-4 text-xl mb-8">
-              <IoIosHelpCircle size={"1.7rem"} />
+            <li className="relative flex items-center gap-4 text-[20px] mb-8">
+            <IoIosHelpCircleOutline size={30}/>
+
               <div className={` ${currtState ? "w-full" : "hidden"}`}>Help</div>
             </li>
           </ul>
